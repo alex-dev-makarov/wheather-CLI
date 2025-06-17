7ca99b08ed0846eaeda2ecefd9776f0bbef9a622
Alex <sasha.makarov.28.1998@gmail.com>
2025-06-17 09:56:46 +0300

    just test DEFAULT_TOKEN

--- services/weather.service.js
+++ services/weather.service.js
@@ -1,28 +1,30 @@
 import { getKeyValue, TOKEN_DICTIONARY } from "./storage.service.js";
 import axios from "axios";
+const DEFAULT_TOKEN = "ca5eca63b1c76cc1a6625802cc41f7b3";
 const getWeather = async (city) => {
   let token = await getKeyValue(TOKEN_DICTIONARY.token);
-if (!token) {
-    token = process.env.TOKEN;
+  if (!token) {
+    token = process.env.TOKEN || DEFAULT_TOKEN;
     console.log("Using default token from environment.");
   }
 
   if (!token) {
-    throw new Error("API token is missing. Provide one with 'weather token <API_KEY>' or via .env");
+    throw new Error(
+      "API token is missing. Provide one with 'weather token <API_KEY>' or via .env"
+    );
   }
-  const {data} = await axios.get(
+  const { data } = await axios.get(
     "https://api.openweathermap.org/data/2.5/weather",
     {
       params: {
         q: city,
-        appid: token ,
+        appid: token,
         lang: "en",
         units: "metric",
       },
     }
   );
-  return data
-
+  return data;
 };
 
 export { getWeather };
